2c040459c5aef07a41730a3eea42642194eb05fc
hugh88 <7hgallagly@stpatrick.org>
2017-04-26 11:24:25 -0500

    Add files via upload

---
+++ simplepass.js
@@ -0,0 +1,16 @@
+//type checkString below
+checkString(str){
+if(str.length < 8 || str.length >20){
+  else(throw new RangeError
+
+}
+
+
+
+//type getString below
+getString(){
+  try {
+    }
+  catch{
+  }
+}
