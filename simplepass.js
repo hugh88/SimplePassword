5901ac6f588149b3ef61e4f7be01de2fa4b511e9
hugh88 <7hgallagly@stpatrick.org>
2017-05-11 00:57:55 -0500

    Add files via upload

--- simplepass.js
+++ simplepass.js
@@ -1,16 +1,21 @@
 //type checkString below
-checkString(str){
-if(str.length < 8 || str.length >20){
-  else(throw new RangeError
-
-}
-
-
-
-//type getString below
-getString(){
-  try {
-    }
-  catch{
+function checkString(str){
+  if(str.length < 8 || str.length > 20){
+    throw new RangeError("Not Valid");
+  }
+  else{
+    return str;
+    console.log("You Good.")
   }
 }
+//type getString below
+function getString(str){
+  try{
+    str = checkString(str);
+  }
+  catch(e){
+    console.log("You Dumb!");
+    getString(str);
+  }
+}
+checkString("Password123");
